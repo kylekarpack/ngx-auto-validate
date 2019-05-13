20cd7db9d9d816d284f476910a033703a729bf69
Kyle Karpack <kylekarpack@gmail.com>
2019-05-13 14:52:02 -0700

    Public API added and generated files removed

--- projects/ngx-auto-validate/src/public-api.ts
+++ projects/ngx-auto-validate/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-auto-validate
  */
 
-export * from './lib/ngx-auto-validate.service';
-export * from './lib/ngx-auto-validate.component';
-export * from './lib/ngx-auto-validate.module';
+export * from "./lib/ngx-auto-validate.module";
