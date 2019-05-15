1edc87edba5292b1418d8df4509fde77ef2c0b89
Kyle Karpack <kylekarpack@gmail.com>
2019-05-15 08:31:42 -0700

    Add testing module

---
+++ projects/ngx-auto-validate-tester/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+
+@Component({
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+	title = "ngx-auto-validate-tester";
+
+	constructor(private formBuilder: FormBuilder) {}
+
+	public form = this.formBuilder.group({
+		required: ["", Validators.required],
+		minLength: ["", Validators.minLength(5)],
+		maxLength: ["", Validators.maxLength(5)],
+		min: ["", Validators.min(5)],
+		max: ["", Validators.max(5)],
+		email: ["", Validators.email],
+		pattern: ["", Validators.pattern(/^[a-z]*$/)],
+		checkboxRequired: ["", Validators.requiredTrue]
+	});
+
+}
